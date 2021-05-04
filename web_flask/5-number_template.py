5d9522b6a9f7e75615736bcb08a0ea1e87aab669
root <root@DESKTOP-EIB5D01.localdomain>
2021-05-04 11:57:10 -0500

    update pep8 style file 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -5,8 +5,10 @@
     Routes:
     /: display “Hello HBNB!”
     /hbnb: display “HBNB”
-    /c/<text>: display “C ”, followed by the value of the text variable (replace underscore _ symbols with a space )
-    /python/(<text>): display “Python ”, followed by the value of the text variable (replace underscore _ symbols with a space )
+    /c/<text>: display “C ”, followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+    /python/(<text>): display “Python ”, followed by the value of the text
+    variable (replace underscore _ symbols with a space )
     The default value of text is “is cool”
     /number/<n>: display “n is a number” only if n is an integer
     /number_template/<n>: display a HTML page only if n is an integer:
@@ -18,16 +20,18 @@ from flask import Flask, escape, render_template
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
 
+@app.route('/', strict_slashes=False)
 def index():
     """ module Hello """
     return 'Hello HBNB!'
 
-@app.route('/hbnb', strict_slashes=False)  
+
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ module Hbnb """
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
@@ -35,17 +39,20 @@ def cisfun(text):
     text = text.replace('_', ' ')
     return 'C %s!' % text
 
+
 @app.route('/python/<text>', strict_slashes=False)
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 def pythontext(text):
     """ module python text """
     text = text.replace('_', ' ')
-    return 'Python ' + text 
+    return 'Python ' + text
+
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def isanumber(n):
     """ module is a number """
     return '{} is a number'.format(n)
+
 
 @app.route('/number_template/<int:num>', strict_slashes=False)
 def number_template(num):
