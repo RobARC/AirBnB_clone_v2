55b7fca65a67943446cc963898421b9a0910ecb8
root <root@DESKTOP-EIB5D01.localdomain>
2021-05-04 11:53:13 -0500

    update pep8 style file 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -5,8 +5,10 @@
     Routes:
     /: display “Hello HBNB!”
     /hbnb: display “HBNB”
-    /c/<text>: display “C ”, followed by the value of the text variable (replace underscore _ symbols with a space )
-    /python/(<text>): display “Python ”, followed by the value of the text variable (replace underscore _ symbols with a space )
+    /c/<text>: display “C ”, followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+    /python/(<text>): display “Python ”, followed by the value of the text
+    variable (replace underscore _ symbols with a space )
     The default value of text is “is cool”
     /number/<n>: display “n is a number” only if n is an integer
     You must use the option strict_slashes=False in your route definition
@@ -16,16 +18,18 @@ from flask import Flask, escape
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
 
+@app.route('/', strict_slashes=False)
 def index():
     """ module Hello """
     return 'Hello HBNB!'
 
-@app.route('/hbnb', strict_slashes=False)  
+
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ module Hbnb """
     return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
@@ -33,18 +37,21 @@ def cisfun(text):
     text = text.replace('_', ' ')
     return 'C %s!' % text
 
+
 @app.route('/python/<text>', strict_slashes=False)
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 def pythontext(text):
     """ module python text """
     text = text.replace('_', ' ')
-    return 'Python ' + text 
+    return 'Python ' + text
+
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def isanumber(n):
     """ module is a number """
 
     return '{} is a number'.format(n)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
