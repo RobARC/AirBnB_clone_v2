37212905680e729f45cde033442c171b81ab6f57
root <root@DESKTOP-EIB5D01.localdomain>
2021-05-04 11:35:42 -0500

    update pep8 style file 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -13,16 +13,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
 
+@app.route('/', strict_slashes=False)
 def index():
     """ module Hello """
     return 'Hello HBNB!'
 
-@app.route('/hbnb', strict_slashes=False)  
+
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ module Hbnb """
     return 'HBNB'
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
